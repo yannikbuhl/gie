d3a0358f00361d502ef8f1f1f8279fffd391aa8d
yannikbuhl <yannik.buhl@posteo.de>
2022-04-18 19:22:53 +0200

    improvement on setnull()#

--- R/utils.R
+++ R/utils.R
@@ -125,8 +125,7 @@ parseresult <- function(raw_results) {
 
   results <- raw_results %>%
     magrittr::extract2("data") %>%
-    purrr::map(., ~ setnull(.x, "info")) %>%
-    purrr::map(., ~ setnull(.x, "children")) %>%
+    purrr::map(., ~ setnull(.x, c("info", "children"))) %>%
     purrr::map_dfr(.f = bind_rows) %>%
     dplyr::arrange(gasDayStart) %>%
     dplyr::mutate(gasDayStart = lubridate::as_date(gasDayStart),
@@ -230,10 +229,7 @@ get_listinghierarchy <- function(raw_results,
     results <- raw_results %>%
       pluck("SSO") %>%
       pluck(region) %>%
-      map(.f = ~ map(.x, .f = ~ setnull(., "facilities"))) %>%
-      map(.f = ~ map(.x, .f = ~ setnull(., "data"))) %>%
-      map(.f = ~ map(.x, .f = ~ setnull(., "image"))) %>%
-      # map(.f = ~ map(.x, .f = ~ setnull(., c("facilities", "data", "image")))) %>%
+      map(.f = ~ map(.x, .f = ~ setnull(., c("facilities", "data", "image")))) %>%
       map_dfr(.f = bind_rows, .id = "country")
 
     return(results)
@@ -245,9 +241,7 @@ get_listinghierarchy <- function(raw_results,
     results <- raw_results %>%
       pluck("SSO") %>%
       pluck(region) %>%
-      map(.f = ~ map(.x, .f = ~ setnull(., "facilities"))) %>%
-      map(.f = ~ map(.x, .f = ~ setnull(., "data"))) %>%
-      map(.f = ~ map(.x, .f = ~ setnull(., "image"))) %>%
+      map(.f = ~ map(.x, .f = ~ setnull(., c("facilities", "data", "image")))) %>%
       map_dfr(.f = bind_rows, .id = "country") %>%
       filter(country == {{ country }})
 
